9a7cd7810f0faa4cc0b8b9d215a0d807029b531d
vasik <vaclav.struhar@mdh.se>
2022-11-02 15:42:02 +0100

    two workloads

--- custom_syscall/custom_syscall.c
+++ custom_syscall/custom_syscall.c
@@ -6,6 +6,7 @@
 
 #include "../kernel/sched/sched.h"
 
+#define TOTAL_BUDGET 5000000
 
 unsigned long timenow(void) {
 	struct timespec timecheck;
@@ -14,27 +15,44 @@ unsigned long timenow(void) {
 	
 }
 
+long long total_budget = TOTAL_BUDGET;
+
 SYSCALL_DEFINE1(struhar_init, long, response_time) {
+	trace_printk("XDEBUG:%d:struhar_init:response_time=%lld\n", current->pid, response_time);
+
 	current->struhar_exp_response_time = response_time;
-	
-	/*if (current->node_controller->containerA_pid == 0) {
+	if (current->node_controller->containerA_pid != 0 && current->node_controller->containerB_pid != 0) {
+		// reset
+		trace_printk("XDEBUG:%d:struhar_init-restart\n", current->pid);		
+		current->node_controller->containerA_pid = 0;
+		current->node_controller->containerB_pid = 0;
+		total_budget = TOTAL_BUDGET;
+	}
+
+
+	if (current->node_controller->containerA_pid == 0) {
 		current->node_controller->containerA_pid = current->pid;
+		
+		trace_printk("XDEBUG:%d:nodes_pid_pidA:id1=%lld\n", current->pid, current->node_controller->containerA_pid);
 	} else {
 		current->node_controller->containerB_pid = current->pid;
-	}*/
+		
+		trace_printk("XDEBUG:%d:nodes_pid_pidB:id1=%lld\n", current->pid, current->node_controller->containerB_pid);
 
+	}
 
-
+	trace_printk("XDEBUG:%d:nodes_pid:id1=%lld:id2=%lld\n", 
+		current->pid, 
+		current->node_controller->containerA_pid, 
+		current->node_controller->containerB_pid);
 }	
 
 SYSCALL_DEFINE2(struhar_xcontrol, long, id, long, total_budget) {
 	trace_printk("XDEBUG:%d:struhar_xcontrol:id=%lld:total_budget=%lld\n", current->pid, id, total_budget);
-	current->node_controller->containerA_pid = 0;
-	current->node_controller->containerB_pid = 0;
 }
 
 void node_control(struct task_struct *p) {
-	trace_printk("XDEBUG:%d:node_controller:id=%lld:id1=%lld:id2=%lld\n", 
+	trace_printk("XDEBUG:%d:node_controller:pidA=%lld:pidB=%lld\n", 
 		current->pid, 
 		current->node_controller->containerA_pid, 
 		current->node_controller->containerB_pid);
@@ -43,18 +61,26 @@ void node_control(struct task_struct *p) {
 	struct sched_dl_entity *dl_se = dl_group_of(rt_rq);
 	long response_time = timenow()-p->struhar_instance_start;
 
-
-	//u64
-	// control logic
-	if (current->struhar_exp_response_time == -1) {
-		trace_printk("XDEBUG:%d:NODE-CONTROLLER-NEW\n", p->pid);
+	if (current->node_controller->containerA_pid == 0 ||
+		current->node_controller->containerB_pid == 0) {
+		trace_printk("XDEBUG:%dnode_controller-nothing-to-control\n", p->pid);
 		return;
 	}
 
+	//u64
+	// control logic
+	/*if (current->struhar_exp_response_time == -1) {
+		trace_printk("XDEBUG:%d:NODE-CONTROLLER-NEW\n", p->pid);
+		return;
+	}*/
 
-	if (current->struhar_exp_response_time < response_time) {
+
+	if (total_budget > 0) {
 		trace_printk("XDEBUG:%d:NODE-CONTROLLER:runtime=%lld\n", p->pid, dl_se->dl_runtime);
-		dl_se->dl_runtime = dl_se->dl_runtime * 105 / 100;  //0.95 
+		int portion = total_budget * 5 / 100;
+		total_budget -= portion;
+
+		dl_se->dl_runtime += portion;  //0.95 
 		trace_printk("XDEBUG:%d:NODE-CONTROLLER:xnew-runtime=%lld\n", p->pid, dl_se->dl_runtime);
 	}
 
@@ -71,11 +97,34 @@ void container_controller(struct task_struct *p) {
 		return;
 	}
 
+	long disturbance = current->struhar_exp_response_time - response_time;
+
 	trace_printk("XDEBUG:%d:CONTROLLER-NEW\n", p->pid);
 	trace_printk("XDEBUG:%d:CONTROLLER\n", p->pid);
 	trace_printk("XDEBUG:%d:CONTROLLER:struhar_exp_response_time=%lld\n", p->pid, p->struhar_exp_response_time);
 	trace_printk("XDEBUG:%d:CONTROLLER:real_response_time=%lld\n", p->pid, response_time);
+	trace_printk("XDEBUG:%d:CONTROLLER:disturbance=%lld\n", p->pid, disturbance);
+	trace_printk("XDEBUG:%d:CONTROLLER:runtime=%lld\n", p->pid, dl_se->dl_runtime);
 
+	if (disturbance > 0) { // lower budget is needed
+		
+		//dl_se->dl_runtime = dl_se->dl_runtime - disturbance/100;  //0.95
+
+
+		if (dl_se->dl_runtime < 5000000) {
+			dl_se->dl_runtime = 5000000;
+		}
+		// trace_printk("XDEBUG:%d:CONTROLLER:xnew-runtime=%lld\n", p->pid, dl_se->dl_runtime);
+	} else{
+		
+		//dl_se->dl_runtime = dl_se->dl_runtime - disturbance/100;
+	}
+
+
+
+	trace_printk("XDEBUG:%d:CONTROLLER:xnew-runtime=%lld\n", p->pid, dl_se->dl_runtime);
+	
+	/*
 	if (current->struhar_exp_response_time > response_time) {
 		trace_printk("XDEBUG:%d:CONTROLLER:runtime=%lld\n", p->pid, dl_se->dl_runtime);
 		dl_se->dl_runtime = dl_se->dl_runtime * 95 / 100;  //0.95
@@ -90,10 +139,7 @@ void container_controller(struct task_struct *p) {
 			dl_se->dl_runtime = 22000000;
 		}
 		trace_printk("XDEBUG:%d:CONTROLLER:xnew-runtime=%lld\n", p->pid, dl_se->dl_runtime);
-	}
-
-
-
+	}*/
 
 }
 
@@ -146,8 +192,8 @@ asmlinkage long sys_struhar_done(void) {
     	current->struhar_exp_response_time,
     	timenow());
 
-	container_controller(current);
-	//node_control(current);
+	//container_controller(current);
+	node_control(current);
 
 	
 	
